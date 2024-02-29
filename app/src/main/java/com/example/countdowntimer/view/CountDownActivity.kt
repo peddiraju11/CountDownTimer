00befdd41eb67d1e6f46798c7be3e3b1c6a3094c
peddiraju <g.peddiraju93@gmail.com>
2024-02-29 13:43:16 +0530

    Added basic counter view

--- app/src/main/java/com/example/countdowntimer/view/CountDownActivity.kt
+++ app/src/main/java/com/example/countdowntimer/view/CountDownActivity.kt
@@ -2,7 +2,10 @@ package com.example.countdowntimer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.countdowntimer.databinding.ActivityCounterBinding
+import com.example.countdowntimer.model.CountDownData
 import com.example.countdowntimer.viewmodel.CountDownViewModel
 
 class CountDownActivity : AppCompatActivity() {
@@ -14,5 +17,19 @@ class CountDownActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCounterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        countDownViewModel = ViewModelProvider(this).get(CountDownViewModel::class.java)
+
+        countDownViewModel.countdown.observe(this, Observer {
+            updateCountdownUI(it)
+        })
+
+        countDownViewModel.startTimer(60) // 60 Sec
+    }
+
+    private fun updateCountdownUI(data: CountDownData) {
+        binding.apply {
+            counterText.text = data.seconds.toString()
+        }
     }
 }
--- app/src/main/java/com/example/countdowntimer/viewmodel/CountDownViewModel.kt
+++ app/src/main/java/com/example/countdowntimer/viewmodel/CountDownViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.countdowntimer.viewmodel
 
+import android.os.CountDownTimer
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,5 +11,36 @@ class CountDownViewModel : ViewModel() {
     private val _countdown = MutableLiveData<CountDownData>()
     val countdown: LiveData<CountDownData>
         get() = _countdown
+
+    private lateinit var timer: CountDownTimer
+
+    fun startTimer(totalSeconds: Long) {
+        timer = object : CountDownTimer(totalSeconds * 1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeRemaining = calculateTimeRemaining(millisUntilFinished)
+                _countdown.postValue(timeRemaining)
+            }
+
+            override fun onFinish() {
+                _countdown.value = CountDownData(0, 0, 0, 0)
+            }
+        }
+        timer.start()
+    }
+
+    private fun calculateTimeRemaining(millisUntilFinished: Long): CountDownData {
+        val totalSeconds = millisUntilFinished / 1000
+
+        val days = totalSeconds / (24 * 3600)
+        val hours = (totalSeconds % (24 * 3600)) / 3600
+        val minutes = ((totalSeconds % 3600) / 60)
+        val seconds = (totalSeconds % 60)
+        return CountDownData(days, hours, minutes, seconds)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
 
 }
